37a1fceb248e0f21c74e3f776d7e7b729963ba18
Tanweer Ahmed <Tanweer@Tanweer.local>
2024-11-07 16:00:22 +0400

    POM Implementation

---
+++ pages/home-page.ts
@@ -0,0 +1,22 @@
+import {type Locator, type Page, expect} from '@playwright/test'
+
+export class HomePage{
+    readonly page: Page;
+    readonly getStartedButton: Locator;
+    readonly title: RegExp;
+
+    constructor(page:Page){
+        this.page = page;
+        this.getStartedButton = page.getByRole('link', { name: 'Get started' });
+        this.title = /Playwright/;
+    }
+
+async clickGetStarted(){
+    await this.getStartedButton.click();
+}
+
+async assertPageTitle(){
+    await expect(this.page).toHaveTitle(this.title)
+}
+
+}
